fa53266822afcafb6c933cb2e7791c9dfde6010c
Aayush Shrivastava <aayushshrivastava508@gmail.com>
2025-09-18 20:51:13 +0530

    Added Cloudinary utils and file upload middleware

---
+++ utils/cloudinary.js
@@ -0,0 +1,25 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs"
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET_KEY
+});
+
+const uploadOnCloudinary=async (localFilePath)=>{
+  try {
+    if(!localFilePath){
+      throw new Error("File path is missing") 
+    }
+    const response=await cloudinary.uploader.upload(localFilePath, {
+      resource_type:"auto"
+    })
+    console.log("file has been uploaded",response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath) // remove locally saved temp files as the upload operation got failed
+  }
+}
+
+module.exports={uploadOnCloudinary};
